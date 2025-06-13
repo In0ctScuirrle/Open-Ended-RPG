77a335fd885d8e2ea0a51bee5ec005365f762aa6
Will Blanchard <157427487+In0ctScuirrle@users.noreply.github.com>
2025-06-13 10:34:16 +0200

    Added the Main file
    Working from outside and IDE for now, so I'm just adding some files manually through GitHub.

---
+++ Campaign-Master/src/main/java/lu/embellishedduck/campaign_master/main/Runner.java
@@ -0,0 +1,16 @@
+package lu.embellishedduck.campaign_master.main;
+
+public class Main {
+
+    // CONSTANTS
+    public static final String GAME_DIRECTORY = System.getProperty("user.home") + "/EmbellishedDuck/Quarklus";
+    public static final String RULES_DIRECTORY = USER_DIRECTORY + "/rules";
+    public static final String CUSTOM_RULES_DIRECTORY = RULES_DIRECTORY + "/custom";
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Campaign Master, you're one stop shop for a smooth and custom TTRPG experience.");
+
+    }//End of Main Method
+
+}//End of Class
